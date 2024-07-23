5ae99aaccec5bb552d0282624d09e424a3be3082
Valentin Zadorozhniy <Zadorozhnaya@mail.ru>
2024-07-23 08:00:50 +0700

    add react-nestjs-mongodb example

---
+++ frontend/src/components/shared/layout/Layout.tsx
@@ -0,0 +1,30 @@
+﻿import React from 'react'
+import { message } from 'antd'
+import { Outlet, useLocation  } from 'react-router-dom'
+
+import Menu from '@app/shared/menu/Menu'
+import Footer from '@app/shared/footer/Footer'
+import { INotification } from './types'
+
+const Layout: React.FC = () =>{
+	
+	const [messageApi, contextHolder] = message.useMessage()
+	const openNotification = ({type, content}: INotification) => {
+		messageApi.open({
+			type: type,
+			content: <>{content}</>,
+		})
+	}
+	const location = useLocation()
+
+	return (
+		<>
+			{contextHolder}
+			<Menu openNotification={openNotification}/>
+			<Outlet context={openNotification} />
+			{location.pathname === '/' ? (<Footer />) : null }
+		</>
+	)
+} 
+
+export default Layout
---
+++ frontend/webpackConfig/webpack.dev.js
@@ -0,0 +1,20 @@
+﻿const { merge } = require('webpack-merge')
+const common = require('../webpack.common.js')
+const path = require('path')
+
+module.exports = merge(common, {
+
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, '../dist'),
+		clean: true,
+		publicPath: '/',
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		static: '../dist',
+		hot: true,
+		port: 8081,
+		historyApiFallback: true, 
+	},
+})
